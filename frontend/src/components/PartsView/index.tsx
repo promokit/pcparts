90ee5d81210ac62556ca4df30ce253ad0f6c726b
Monika Kłokosińska <monika.klokosinska@intive.com>
2023-02-26 17:50:15 +0100

    feat: add hard-coded svg, update README

---
+++ frontend/src/components/PartsView/index.tsx
@@ -0,0 +1,26 @@
+import styles from './partsview.module.css';
+
+function PartsView() { 
+    return (<div className={styles.view}>
+            <svg viewBox="0 0 500 500" className={styles.svg}>
+                <rect x="10" y="10" width="480" height="480" rx="5" stroke-width="3" ry="5" stroke="#000" fill="#fff" />
+
+                <rect x="40" y="50" width="60" height="30" className={styles.fitting} />
+                <rect x="40" y="90" width="60" height="30" strokeWidth="2" stroke="#666" fill="#fff" />
+                <rect x="40" y="130" width="60" height="30" stroke="#666" strokeWidth="2" fill="#fff" />
+                <rect x="40" y="170" width="60" height="30" stroke="#666" strokeWidth="2" fill="#fff" className={styles.movingLine}/>
+
+                <rect x="120" y="220" width="100" height="100" rx="3" ry="3" fill="#fff" strokeWidth="2" stroke="#000"/>
+
+                <rect x="280" y="50" width="120" height="240" fill="#fff" strokeWidth="2" stroke="#1cf" />
+
+                <circle cx="400" cy="400" r="60" strokeWidth="2" stroke="#555" fill="#fff" />
+
+                <rect x="36" y="86" width="68" height="38"  opacity="0.8" className={styles.notFitting} />
+               
+            </svg>
+    </div>)
+}
+
+
+export default PartsView;
---
+++ frontend/src/components/Selector/index.tsx
@@ -0,0 +1,7 @@
+import styles from './selector.module.css';
+
+function Selector() {
+    return <div className={styles.selector}>Selector</div>
+}
+
+export default Selector;
