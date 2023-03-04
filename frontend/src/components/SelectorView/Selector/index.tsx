18af4891b4db7633a277af33411c6e541a5c2147
Monika Kłokosińska <monika.klokosinska@intive.com>
2023-03-04 21:06:27 +0100

    feat: add generic select component

--- frontend/src/components/SelectorView/Selector/index.tsx
+++ frontend/src/components/SelectorView/Selector/index.tsx
@@ -6,10 +6,9 @@ interface SelectorProps<ElementType> {
 
 
 function Selector<ElementType extends { model: string, brand: string}>({ options, type, label }: SelectorProps<ElementType>) {
-    console.log('!!!,', options)
     return <div><label htmlFor={type}>{label}</label>
         <select name={label} id={label}>
-            {options.map(({ model, brand }) => (<option>{`${model} (${brand})`}</option>))}
+            {options.map(({ model, brand }) => (<option key={model}>{`${model} (${brand})`}</option>))}
         </select>
     </div>
 }
